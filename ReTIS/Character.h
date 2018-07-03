b7e4ab818a8a5e52faf0b8e6d168942b8656d42d
music <music@DESKTOP-2HFN8KI>
2018-07-04 00:32:08 +0900

    敵1体作成 着地時に判定を勝手に作りましtた

--- ReTIS/Character.h
+++ ReTIS/Character.h
@@ -7,6 +7,10 @@ protected:
 	float	jump = 0.f, gravity = 1.f , inertia = 0; // 重力と慣性
 	bool	possess;	// 乗り移られているか
 	void	Physical();	// ジャンプとかの計算
+
+	// 着地した時の判定 ------------------
+	bool	landing;
+
 public:
 	cCharacterBase() {};
 	~cCharacterBase() {};
@@ -36,14 +40,14 @@ class cEnemy : public cCharacterBase {
 protected:
 	cBulletManager *bullet;
 public:
-	cEnemy(float x, float y, float w, float h, float s, bool p) {
+	/*cEnemy(float x, float y, float w, float h, float s, bool p) {
 		pos     = { x, y, 0.f };
 		size    = { w, h, 0.f };
 		speed   = s;
 		possess = p;
 		bullet  = new cBulletManager();
 		type    = Enemy;
-	}
+	}*/
 	~cEnemy() {
 		delete bullet;
 		bullet = nullptr;
@@ -51,12 +55,98 @@ public:
 	void	Render();
 };
 
+class cEnemyJumpman : public cEnemy 
+{
+public:
+	cBulletManager *bullet;
+
+	float rad;
+	bool move_flag;
+	int jump_count;
+	int move_dir;
+
+	cEnemyJumpman(float x, float y, float w, float h, float s, bool p) {
+		pos = { x, y, 0.f };
+		size = { w, h, 0.f };
+		speed = s;
+		possess = p;
+		bullet = new cBulletManager();
+		type = Enemy;
+		landing = false;
+		move_flag = false;
+		jump_count = 0;
+		move_dir = 1;
+	}
+	~cEnemyJumpman() {
+		delete bullet;
+		bullet = nullptr;
+	}
+	
+	void Update();
+	void move();
+	void MoveByAutomation();
+};
+
+class cEnemyGunman : public cEnemy
+{
+public:
+	int attack_count;
+
+
+	cEnemyGunman(float x, float y, float w, float h, float s, bool p) {
+		pos = { x, y, 0.f };
+		size = { w, h, 0.f };
+		speed = s;
+		possess = p;
+		bullet = new cBulletManager();
+		type = Enemy;
+		landing = false;
+
+		attack_count = 0;
+	}
+
+	void Update();
+	void move();
+	void MoveByAutomation();
+};
+
+class cEnemyCannon : public cEnemy
+{
+public:
+	int attack_count;
+
+
+	cEnemyCannon(float x, float y, float w, float h, float s, bool p) {
+		pos = { x, y, 0.f };
+		size = { w, h, 0.f };
+		speed = s;
+		possess = p;
+		bullet = new cBulletManager();
+		type = Enemy;
+		landing = false;
+
+		attack_count = 0;
+	}
+
+	void Update();
+	void move();
+	void MoveByAutomation();
+};
+
+
 class cCharacterManager {
 protected:
 public:
 	cPlayer *player;
+	cEnemyJumpman *jumpman;
+	cEnemyGunman *gunman;
+	cEnemyCannon * cannon;
 	cCharacterManager() {
 		player = new cPlayer(400.f, 100.f, 90.f, 120.f, 6.f, true);
+		jumpman = new cEnemyJumpman(300.f, 100.f, 90.f,120.f, 2.f, false);
+		gunman = new cEnemyGunman(200.f, 100.f, 90.f, 120.f, 2.f, false);
+		cannon = new cEnemyCannon(200.f, 100.f, 90.f, 120.f, 2.f, false);
+
 	}
 	~cCharacterManager() {
 		delete player;
@@ -65,4 +155,7 @@ public:
 	void	Update();
 	void	Render();
 	cObject *GetPlayer() { return (cObject*)player; }
+	cObject *GetEnemyJumpman() { return (cObject*)jumpman; }
+	cObject *GetEnemyGunman() { return (cObject*)gunman; }
+	cObject *GetEnemyCannon() { return (cObject*)cannon; }
 };
