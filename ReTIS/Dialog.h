ef1754b37463ae165ebf6234a8a4a89b9bac819e
藤井 将人 <marisa_fast@yahoo.co.jp>
2018-09-18 05:45:23 +0900

    a

--- ReTIS/Dialog.h
+++ ReTIS/Dialog.h
@@ -9,7 +9,7 @@ class cDialog {
 public:
 	// 変数
 	string	message;			// メッセージ文字列全文
-	string	out;				// 出力されている文字列
+	string	out;				// 出力されている文字列a
 	clock_t wait_time = 50;		// オフセットタイム
 	clock_t draw_time = 3000;	// 全文字列表示してから消えるまで
 	clock_t start = 0, end = 0; // オフセット計測用変数
